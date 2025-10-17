fde06bd851b1e40e8b82b27544cfc07c9aedc475
arkhann00 <khaarsyan@gmail.com>
2025-10-17 23:48:17 +0300

    Удаление кнопки Событий и обновление ссылки на анкету

--- src/keyboards.py
+++ src/keyboards.py
@@ -2,10 +2,10 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeybo
 
 def main_keyboard():
     return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Карта МП 🗺️", callback_data="map"),
-         InlineKeyboardButton(text="Ближайшие события 🔥", callback_data="events")],
+        [InlineKeyboardButton(text="Карта МП 🗺️", callback_data="map"),]
+         # InlineKeyboardButton(text="Ближайшие события 🔥", callback_data="events")],
         [InlineKeyboardButton(text="Вступить в МП ⚡️",
-                              url="https://docs.google.com/forms/d/e/1FAIpQLSdlqzArM0zpNGjoPxheXcLzu0pScaU56315Qtx0JLdgUXie0g/viewform")],
+                              url="https://forms.gle/DEYmN5mJfHeH6XpU8")],
     ], resize_keyboard=True)
 
 def back_keyboard():
